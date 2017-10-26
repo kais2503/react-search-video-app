4ee5862d670d2ead42250bb1d436a91daee8adf4
kaies labiedh <kaies.labiedh@gmail.com>
2017-10-26 22:19:39 +0200

    add readme.md

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
 'use strict';
+import React, { Component } from 'react'
 import SearchBar from './components/search_bar';
 import ReactDOM from 'react-dom';
-import React from 'react';
+import YTSearch from 'youtube-api-search';
 
-const API_KEY= 'AIzaSyBNN903EZRtJMBiBg9QplZA5g_mBuROIYg'
+const API_KEY = 'AIzaSyBNN903EZRtJMBiBg9QplZA5g_mBuROIYg'
+
 //App is a class
-const App = () => {
-    return ( 
-    <div> 
-        <SearchBar />
+class App extends Component {
+    constructor(props) {
+        super(props)
+       
+        YTSearch({ key: API_KEY, term: 'surfboards' }, function (data) {
+            this.setState( { videos: data });
+        })
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
 
-    </div>
-    );
+            </div>
+        );
+    }
+
 }
 //ReactDOM should takes an instance a first argument <App /> is a jsx instance and target as second element "the place holder"
 ReactDOM.render(<App />, document.querySelector('.container'));
