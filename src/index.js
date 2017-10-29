c498f099cda37acd353dd0bee1fa31a6cb3a4c32
kaies labiedh <kaies.labiedh@gmail.com>
2017-10-29 16:46:28 +0100

    video_list and video_list_item component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 'use strict';
 import React, { Component } from 'react'
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list'
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
@@ -9,16 +10,17 @@ const API_KEY = 'AIzaSyBNN903EZRtJMBiBg9QplZA5g_mBuROIYg'
 //App is a class
 class App extends Component {
     constructor(props) {
-        super(props)
-       
-        YTSearch({ key: API_KEY, term: 'surfboards' }, function (data) {
-            this.setState( { videos: data });
+        super(props);
+        this.state={videos:[]};
+        YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+            this.setState({ videos });
         })
     }
     render() {
         return (
             <div>
                 <SearchBar />
+                <VideoList videos ={this.state.videos}/>
 
             </div>
         );
